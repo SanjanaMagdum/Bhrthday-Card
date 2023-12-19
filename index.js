f59242ceb523fc8bad9778da9bc80950f31b9804
SanjanaMagdum <136708515+SanjanaMagdum@users.noreply.github.com>
2023-12-19 16:32:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const Data =[{
+    "name":"Hrithik Roshan",
+    "age":27,
+    "image":"https://assets.gqindia.com/photos/5fdc6eb81c519f6c10ba2d2f/16:9/w_2560%2Cc_limit/Hrithik-Roshan%2520(2).jpg"
+},
+{
+    "name":"shah rukh khan",
+    "age":28,
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMT_6cdc5u0sACt0x7vkmiRxFxzUhQoQ8bI1--G1FSZidwYEosB8PwpHCut1PS4Hurvfk&usqp=CAU"
+},
+{
+    "name":"akshay kumar",
+    "age":29,
+    "image":"https://in.bmscdn.com/iedb/artist/images/website/poster/large/akshay-kumar-94-1681713982.jpg"
+},
+{
+    "name":"varun",
+    "age":35,
+    "image":""
+    
+}
+
+]
